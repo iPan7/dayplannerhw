474012e7b83a6312373447b07a8b5d6186550c6d
Ian Panlilio <ipanlilio7@gmail.com>
2020-03-31 20:01:15 -0700

    addedjstimecolorframereference

--- Assets/script.js
+++ Assets/script.js
@@ -25,7 +25,8 @@ setColors ();
 function setup (){
     eventField["0900"] = document.getElementById("0900");
     eventField["1000"] = document.getElementById("1000");
-
+    eventField["1100"] = document.getElementById("1100");
+    eventField["1200"] = document.getElementById("1200");
 }
 
 function addEvent (clickId){
@@ -78,97 +79,51 @@ function clearEvent (clickId){
 
 function setColors (){
     let time = moment().format('LT');
-    console.log(time);
-    eventField["0900"].classList.add("old");
+    let hour = time.split(":")[0];
+    var AmPm = time.slice(-2);
+    // var milTimeHour = hour + (AmPm == "PM" ? 12 : 0);
+    var milTimeHour = 10;
+console.log(milTimeHour);
+
+    //check time
+    //checking 0900
+    if (milTimeHour > 9){
+        eventField["0900"].classList.add("old");
+    }
+    else if (milTimeHour == 9){
+        eventField["0900"].classList.add("current");
+    }
+    else {
+        eventField["0900"].classList.add("new");
+    }
+    //checking 1000
+    if (milTimeHour > 10){
+        eventField["1000"].classList.add("old");
+    }
+    else if (milTimeHour == 10){
+        eventField["1000"].classList.add("current");
+    }
+    else {
+        eventField["1000"].classList.add("new");
+    }
+    //checking 1100
+        if (milTimeHour > 11){
+        eventField["1100"].classList.add("old");
+    }
+    else if (milTimeHour == 11){
+        eventField["1100"].classList.add("current");
+    }
+    else {
+        eventField["1100"].classList.add("new");
+    }
+    //checking 1200
+        if (milTimeHour > 12){
+        eventField["1200"].classList.add("old");
+    }
+    else if (milTimeHour == 12){
+        eventField["1200"].classList.add("current");
+    }
+    else {
+        eventField["1200"].classList.add("new");
+    }
 }
-
-// function to color day planner based on current time
-
-// function to cause save button to save event to local storage
-
-// function to display event from local storage in event field
-
-// function to clear event from event field and local storage
-
-//
-
-// $( "" ).addClass( "green" );
-// $(document).ready(function() {
-// });
-
-// var tasks = document.getElementById("tasks");
-// let tasks = JSON.parse(localStorage.getItem("tasks"));
-// localStorage.setItem("tasks")
-
-// const saveBtn = document.getElementsByClassName("saveBtn");
-// addEvent1.addEventListener('click', addEvent, task1);
-// addEvent2.addEventListener('click', addEvent);
-// addEvent3.addEventListener('click', addEvent);
-// addEvent4.addEventListener('click', addEvent);
-// addEvent5.addEventListener('click', addEvent);
-// addEvent6.addEventListener('click', addEvent);
-// addEvent7.addEventListener('click', addEvent);
-// addEvent8.addEventListener('click', addEvent);
-// addEvent9.addEventListener('click', addEvent);
-
-// const field1 = document.querySelector("field1");
-
-// function task1(){
-//     console.log(task1);
-//     let field1_seriaized = JSON.stringify(field1)
-//     console.log(field1_seriaized);
-
-//     localStorage.setItem("field1", field1_serialized)
-//     console.log(localstorage);
-// }
-// let field1_deserialized = JSON.parse(localStorage.getItem("field1"));
-
-// console.log(field1_deserialized);
-
-// function addEvent(){
-//     alert("task saved!");
-// }
-
-
-//   // function to update row color
-//   function updateRowColor ($hourRow,hour) { 
-
-//     if (test) { console.log("rowColor ",nowHour24, hour); }
-
-//     if ( hour < nowHour24) {
-//       // $hourRow.css('')
-//       if (test) { console.log("lessThan"); }
-//       $hourRow.css("background-color","lightgrey")
-//     } else if ( hour > nowHour24) {
-//       if (test) { console.log("greaterthan"); }
-//       $hourRow.css("background-color","lightgreen")
-//     } else {
-//       if (test) { console.log("equal"); }
-//       $hourRow.css("background-color","tomato")
-//     }
-//   };
-
-
-//   // saves to local storage
-//   // onclick function to listen for user clicks on plan area
-//   $(document).on('click','i', function(event) {
-//     event.preventDefault();  
-
-//     if (test) { console.log('click pta before '+ planTextArr); }
-
-//     let $index = $(this).attr('save-id');
-
-//     let inputId = '#input-'+$index;
-//     let $value = $(inputId).val();
-
-//     planTextArr[$index] = $value;
-
-
-//     if (test) { console.log('value ', $value); }
-//     if (test) { console.log('index ', $index); }
-//     if (test) { console.log('click pta after '+ planTextArr); }
-
-//     // remove shawdow pulse class
-//     $(`#saveid-${$index}`).removeClass('shadowPulse');
-//     localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
-//   });  
