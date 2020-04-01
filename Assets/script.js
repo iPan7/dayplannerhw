ba24b868b768b521e388800cef4844a9b2c0cc7d
Ian Panlilio <ipanlilio7@gmail.com>
2020-03-31 20:57:37 -0700

    centeredbuildwithsomecleanupofcode

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,8 @@
-// create time and date at top
+//set time with moment.js
 
-let d = new Date()
-
-let year = d.getFullYear();
-let month = d.getMonth()+1;
-let day = d.getDate();
-
-let output =
-(month<10 ? '0' : '') + month + '/' +
-(day<10 ? '0' : '') + day + '/' + year;
-
-$('.date').text(output);
+const m = moment();
+let $currentTime = m.format("LLLL");
+$("#currentDay").text($currentTime);
 
 // setup
 
